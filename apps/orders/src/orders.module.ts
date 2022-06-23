4e7dc4bcd10847eeed49495039620e1bf66eae0c
Meljohn <ugaddanmeljohn@gmail.com>
2022-06-24 02:14:57 +0800

    Initial Commit

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from 'libs/common/src';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
